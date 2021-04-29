72a78690388fde8d61cbb5b2b38a636b031e48cd
AlexSuth <AlexanderNSutherland@gmail.com>
2021-04-28 19:41:26 -0700

    adds shop page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HatsPage from './pages/hatspage/hatspage.component';
 import HomePage from './pages/homepage/homepage.component';
-import JacketsPage from './pages/jacketspage/jacketspage.component';
-import MensPage from './pages/menspage/menspage.component';
-import SneakersPage from './pages/sneakerspage/sneakerspage.component';
-import WomensPage from './pages/womenspage/womenspage.component';
+
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
@@ -15,11 +12,13 @@ function App() {
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
+
+        {/* <Route path='/shop/hats' component={HatsPage} />
         <Route path='/shop/jackets' component={JacketsPage} />
         <Route path='/shop/sneakers' component={SneakersPage} />
         <Route path='/shop/mens' component={MensPage} />
-        <Route path='/shop/womens' component={WomensPage} />
+        <Route path='/shop/womens' component={WomensPage} /> */}
       </Switch>
     </div>
   );

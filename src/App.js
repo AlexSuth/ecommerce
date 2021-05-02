88c85fe76389a65d9c67c8cbb5bce34ea6fba3bb
AlexSuth <AlexanderNSutherland@gmail.com>
2021-05-01 20:04:57 -0700

    creates functional header with links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
-
 import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
@@ -10,15 +10,10 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
-
-        {/* <Route path='/shop/hats' component={HatsPage} />
-        <Route path='/shop/jackets' component={JacketsPage} />
-        <Route path='/shop/sneakers' component={SneakersPage} />
-        <Route path='/shop/mens' component={MensPage} />
-        <Route path='/shop/womens' component={WomensPage} /> */}
       </Switch>
     </div>
   );
